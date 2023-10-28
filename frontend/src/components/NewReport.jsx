1413ae3c366f1f5532cd84e66e432d4c082053e1
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-10-28 17:50:57 +0530

    feat : body injury marking feature by click

--- frontend/src/components/NewReport.jsx
+++ frontend/src/components/NewReport.jsx
@@ -3,19 +3,23 @@ import BackBodyImg from '../assets/back_body.png'
 import FrontBodyImg from "../assets/front_body.png"
 import {useResponsiveScreen} from './Context/ResponsiveScreenContext'
 import "../NewReport.css"
-import { useState,useEffect } from 'react'
+import { useState} from 'react'
 const NewReport = () => {
   const ResponsiveScreenWindow = useResponsiveScreen();
-  const [InjuryPoints, setInjuryPoints] = useState([]);
-  const addPoints = (event)=>{
-    const rect = event.target.getBoundingClientRect();
-    let x = event.clientX - rect.left; //x position within the element.
-let y = event.clientY - rect.top;  //y position within the element.
-let width = rect.right - rect.left;// width of element
-let height = rect.bottom - rect.top;// height of element
-let xp =((x*100)/width) ; // x position as % of element 's width from left most edge of element
-let yp = ((y*100)/height);// y position as % of element 's height from top most edge of element
-setInjuryPoints([{left:`${xp}%`,top:`${yp}%`},...InjuryPoints])
+  const gridElementClassList = Array.from(Array(8192),()=>'cell');
+  const [BackInjuryPoints, setBackInjuryPoints] = useState(gridElementClassList);
+  const toggleBackPoints = (event)=>{
+  const BackGridElementClassList = [...BackInjuryPoints];
+  const index =  parseInt((event.target.id).slice(1));
+   BackGridElementClassList[index]=(BackGridElementClassList[index]==="cell")?"cell circle_Dot":"cell";
+   setBackInjuryPoints(BackGridElementClassList);
+} 
+const [FrontInjuryPoints, setFrontInjuryPoints] = useState([...gridElementClassList]);
+const toggleFrontPoints = (event)=>{
+  const FrontGridElementClassList = [...FrontInjuryPoints];
+  const index =  parseInt((event.target.id).slice(1));
+   FrontGridElementClassList[index]=(FrontGridElementClassList[index]==="cell")?"cell circle_Dot":"cell";
+   setFrontInjuryPoints(FrontGridElementClassList);
 }
 const calcWidth=()=>{
   return ResponsiveScreenWindow.windowWidth>900?Math.round((ResponsiveScreenWindow.windowWidth-150 )/2):(ResponsiveScreenWindow.windowWidth>660?Math.round((ResponsiveScreenWindow.windowWidth-100 )):Math.round((ResponsiveScreenWindow.windowWidth-50 )));
@@ -28,14 +32,15 @@ const calcHeight = ()=>ResponsiveScreenWindow.windowWidth>900?Math.round((Respon
     <Box direction='row' wrap justify="between" style={{marginTop:"auto"}}>
     <Box align="center" elevation="" style={{border:"2px solid #7D4CDB",borderRadius:"20px"}}> 
         <Heading level={4} > Back body section</Heading>
-        <Box onClick={(event)=>{ addPoints(event)}}  style={{borderRadius:"20px",backgroundImage:`url(${BackBodyImg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
-         {InjuryPoints.map(({top,left},index)=><span key={index} className='circle_Dot' style={{top:top,left:left}}></span>)}
+        <Box  className="grid" style={{borderRadius:"20px",backgroundImage:`url(${BackBodyImg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
+         {BackInjuryPoints.map((classNames,index)=><span onClick={toggleBackPoints} id={`b${index}`} key={index} className={classNames} ></span>)}
         </Box></Box>
        
         <Box align="center" elevation="" style={{border:"2px solid #7D4CDB",borderRadius:"20px"}}> 
         <Heading level={4} > Front body section</Heading>
-        <Box  style={{backgroundImage:`url(${FrontBodyImg})`,borderRadius:"20px",backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
-       
+        <Box  className="grid" style={{backgroundImage:`url(${FrontBodyImg})`,borderRadius:"20px",backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
+        {FrontInjuryPoints.map((classNames,index)=><span onClick={toggleFrontPoints } id={`f${index}`} key={index} className={classNames} ></span>)}
+      
         </Box>
         
         </Box>
