9c1bf41a63db415fe0ac3491c6dee8da46642254
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-10-28 16:44:24 +0530

    fix : body image size and responsiveness

--- frontend/src/components/NewReport.jsx
+++ frontend/src/components/NewReport.jsx
@@ -3,7 +3,7 @@ import BackBodyImg from '../assets/back_body.png'
 import FrontBodyImg from "../assets/front_body.png"
 import {useResponsiveScreen} from './Context/ResponsiveScreenContext'
 import "../NewReport.css"
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 const NewReport = () => {
   const ResponsiveScreenWindow = useResponsiveScreen();
   const [InjuryPoints, setInjuryPoints] = useState([]);
@@ -17,20 +17,24 @@ let xp =((x*100)/width) ; // x position as % of element 's width from left most 
 let yp = ((y*100)/height);// y position as % of element 's height from top most edge of element
 setInjuryPoints([{left:`${xp}%`,top:`${yp}%`},...InjuryPoints])
 }
+const calcWidth=()=>{
+  return ResponsiveScreenWindow.windowWidth>900?Math.round((ResponsiveScreenWindow.windowWidth-150 )/2):(ResponsiveScreenWindow.windowWidth>660?Math.round((ResponsiveScreenWindow.windowWidth-100 )):Math.round((ResponsiveScreenWindow.windowWidth-50 )));
+}
+const calcHeight = ()=>ResponsiveScreenWindow.windowWidth>900?Math.round((ResponsiveScreenWindow.windowWidth-150 )):(ResponsiveScreenWindow.windowWidth>660?2*Math.round((ResponsiveScreenWindow.windowWidth-100 )):2*Math.round((ResponsiveScreenWindow.windowWidth-50 )));
   return (
-    <Box style={{minHeight:"calc( 100vh - 60px )"}}>
+    <Box style={{minHeight:"calc( 100vh - 60px )", padding:(ResponsiveScreenWindow.windowWidth>660?"0 50px":"0 20px")}} >
    <Heading level={1} align="center" style={{maxWidth:"100vw"}}>Generate new report</Heading>
    <Heading level={3} weight="lighter" align="center" style={{maxWidth:"100vw"}}>Mark the injuries on below body images by clicking the injured areas</Heading>
     <Box direction='row' wrap justify="between" style={{marginTop:"auto"}}>
     <Box align="center" elevation="" style={{border:"2px solid #7D4CDB",borderRadius:"20px"}}> 
         <Heading level={4} > Back body section</Heading>
-        <Box onClick={(event)=>{ addPoints(event)}}  style={{backgroundImage:`url(${BackBodyImg})`,backgroundRepeat:"no-repeat",backgroundSize:"contain",height:`${(ResponsiveScreenWindow.windowWidth*313)/350 - 48}px`,width:`${(ResponsiveScreenWindow.windowWidth)/2 -50}px`}}>
+        <Box onClick={(event)=>{ addPoints(event)}}  style={{borderRadius:"20px",backgroundImage:`url(${BackBodyImg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
          {InjuryPoints.map(({top,left},index)=><span key={index} className='circle_Dot' style={{top:top,left:left}}></span>)}
         </Box></Box>
        
         <Box align="center" elevation="" style={{border:"2px solid #7D4CDB",borderRadius:"20px"}}> 
         <Heading level={4} > Front body section</Heading>
-        <Box  style={{backgroundImage:`url(${FrontBodyImg})`,backgroundRepeat:"no-repeat",backgroundSize:"contain",height:`${(ResponsiveScreenWindow.windowWidth*313)/350 - 48}px`,width:`${(ResponsiveScreenWindow.windowWidth)/2 -50}px`}}>
+        <Box  style={{backgroundImage:`url(${FrontBodyImg})`,borderRadius:"20px",backgroundRepeat:"no-repeat",backgroundSize:"cover",height:`${calcHeight()}px`,width:`${calcWidth()}px`,alignSelf:"center"}}>
        
         </Box>
         
