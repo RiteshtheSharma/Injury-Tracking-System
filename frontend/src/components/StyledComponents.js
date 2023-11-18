b404dda217b6a4d2f3b816867485dbaf1f6aa96c
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-11-18 16:25:05 +0530

    feat : profile imgmodifiable edit comp  created In StyledComponents  used as a pen icon to select new profile img

--- frontend/src/components/StyledComponents.js
+++ frontend/src/components/StyledComponents.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { TableCell,Skeleton } from "grommet";
 import { Link } from "react-router-dom";
+
+
 const ModTableCell = styled(TableCell)`
 {
   padding:0;
@@ -24,5 +26,18 @@ color:black;
 const ModLinearSkeleton =styled(Skeleton)`{
   margin-top:2px;
 }`
+/* settings components */
+const ModPencilIconLabel = styled.label`{
+   display: inline-block;
+  width: 25px;
+  height: 25px;
+  background-image: url('/src/assets/pen.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
+  & > input {visibility:hidden}
+}`
 
-export {ModLink,ModTableCell,ModLinearSkeleton,TableBodyWithScrollBar};
+
+
+export {ModLink,ModTableCell,ModLinearSkeleton,TableBodyWithScrollBar,ModPencilIconLabel};
