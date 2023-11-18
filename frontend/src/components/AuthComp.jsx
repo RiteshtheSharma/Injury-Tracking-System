f0db18f9464cc1a485d1aaf34c3871f009e555fa
RiteshtheSharma <72566311+RiteshtheSharma@users.noreply.github.com>
2023-11-18 16:42:44 +0530

    Refactor : made margin customizable in AuthComp

--- frontend/src/components/AuthComp.jsx
+++ frontend/src/components/AuthComp.jsx
@@ -3,17 +3,17 @@ import { Google } from 'grommet-icons';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useResponsiveScreen } from './Context/ResponsiveScreenContext';
-const AuthComp = ({type,children}) => {
+const AuthComp = ({type,children,margin}) => {
 const handleSignInWithGoogle = ()=>{};
 const responsiveWidth = useResponsiveScreen();
   return (
 
-    <Card  height={((responsiveWidth .windowWidth>450)?"medium":"100%")}  width={((responsiveWidth .windowWidth>450)?"medium":"100%")} background="light-1" style={{margin:"auto"}}>
+    <Card  height={((responsiveWidth .windowWidth>450)?"medium":"100%")}  width={((responsiveWidth .windowWidth>450)?"medium":"100%")} background="light-1" style={{margin:margin}}>
       <CardHeader margin="auto"><h1>{type}</h1></CardHeader>
       <CardBody pad="medium"  justify='evenly'>
       <Button
         icon={<Google color='plain'  />}
-        label={`${type} with Google`}
+        label={`${(type==="Set")?"set new email":type} with Google`}
         onClick={handleSignInWithGoogle}
         style={{marginBottom:"10px"}}
       />
@@ -24,5 +24,7 @@ const responsiveWidth = useResponsiveScreen();
     </Card>
   )
 }
-
+AuthComp.defaultProps = {
+  medium :"auto"
+} 
 export default AuthComp
